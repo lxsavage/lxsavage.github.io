f79361a842fe191e4be1be5edfe040a84750ed51
lxsavage <31578557+lxsavage@users.noreply.github.com>
2023-02-22 16:39:38 -0500

    Began to decouple user banner, added tailwind

---
+++ components/GithubUserBanner.vue
@@ -0,0 +1,35 @@
+<template>
+  <section v-if="state.isLoading">
+    <h1>Loading...</h1>
+  </section>
+  <section v-else>
+    <img :src="githubUser.avatar_url" :alt="`Profile photo of ${githubUser.name}`" />
+    <h1>
+      <a :href="githubUser.html_url" style="text-decoration: none; color: black">
+        {{ githubUser.name }}
+      </a>
+    </h1>
+    <p>{{ githubUser.bio }}</p>
+    <ul>
+      <li>Followers: {{ githubUser.followers }}</li>
+      <li>Following: {{ githubUser.following }}</li>
+    </ul>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+// TODO: Move API fetching logic here
+const props = defineProps({
+  githubUser: Object,
+})
+const state = reactive({
+  isLoading: true
+})
+
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,12 @@
 <template>
   <div>
-    <section v-if="state.pageLoading"></section>
-    <section v-else>
-        <img :src="state.githubUser.avatar_url" :alt="`Profile photo of ${state.githubUser.name}`" />
-        <h1>
-          <a :href="state.githubUser.html_url" style="text-decoration: none; color: black">
-            {{ state.githubUser.name }}
-          </a>
-        </h1>
-        <p>{{ state.githubUser.bio }}</p>
-        <ul>
-          <li>Followers: {{ state.githubUser.followers }}</li>
-          <li>Following: {{ state.githubUser.following }}</li>
-        </ul>
-    </section>
+    <GithubUserBanner :props="githubUser" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import GithubUserBanner from "~/components/GithubUserBanner.vue";
+
 const BASE_URL = 'https://api.github.com/users/';
 const name = 'index.vue';
 
