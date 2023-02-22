8f818a34534f6aa1d87d9f6716e2850f4866ae4c
lxsavage <31578557+lxsavage@users.noreply.github.com>
2023-02-22 17:36:56 -0500

    Implemented GithubUserBanner component

--- components/GithubUserBanner.vue
+++ components/GithubUserBanner.vue
@@ -1,35 +1,38 @@
 <template>
-  <section v-if="state.isLoading">
-    <h1>Loading...</h1>
-  </section>
-  <section v-else>
-    <img :src="githubUser.avatar_url" :alt="`Profile photo of ${githubUser.name}`" />
-    <h1>
-      <a :href="githubUser.html_url" style="text-decoration: none; color: black">
-        {{ githubUser.name }}
-      </a>
-    </h1>
-    <p>{{ githubUser.bio }}</p>
-    <ul>
-      <li>Followers: {{ githubUser.followers }}</li>
-      <li>Following: {{ githubUser.following }}</li>
-    </ul>
-  </section>
+  <div class="md:container mx-auto md:max-w-md rounded-lg md:shadow p-5 mt-5 mb-5">
+    <section v-if="state.isLoading">
+      <p class="text-center">Loading GitHub information for {{ props.username }}...</p>
+    </section>
+    <div v-else class="gap-4 columns-2">
+      <img class="h-32 w-auto rounded-full" :src="state.githubUser.avatar_url" :alt="`Profile photo of ${state.githubUser.name}`" />
+      <div>
+        <h1 class="text-2xl">
+          <a :href="state.githubUser.html_url" style="text-decoration: none; color: black">
+            {{ state.githubUser.name }}
+          </a>
+        </h1>
+        <p>{{ state.githubUser.bio }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from 'vue';
-
-// TODO: Move API fetching logic here
 const props = defineProps({
-  githubUser: Object,
+  username: String,
 })
 const state = reactive({
-  isLoading: true
+  isLoading: true,
+  githubUser: {},
 })
 
+fetch(`https://api.github.com/users/${encodeURIComponent(props.username as string)}`)
+    .then(async result => {
+      state.githubUser = await result.json()
+      state.isLoading = false
+    })
+    .catch(err => {
+      console.log('Failed to fetch res due to error')
+    })
+
 </script>
-
-<style scoped>
-
-</style>
