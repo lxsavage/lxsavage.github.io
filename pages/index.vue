96c053532c7d56f693ee5abc80e6b86770d9ff88
lxsavage <31578557+lxsavage@users.noreply.github.com>
2023-02-24 18:44:10 -0500

    Added about me section

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <template>
   <section>
-    <p class="text-2xl mb-2 text-center">
+    <p class="mb-2 text-center">
       Hi! I'm Logan, an aspiring Software Developer at The Ohio State University.
     </p>
-    <p class="text-center mb-2">
-      Throughout my prior work experience, I have had experience developing full-stack applications using .NET Core and
-      Vue.js for various internal applications.
+    <h1 class="text-2xl mb-2">About Me</h1>
+    <p class="indent-8 mb-2">
+      I have had a fascination with computers for as long as I can remember, and knew that what I would do when I grew
+      up would involve them in some way. Over the course of high school, I was exposed to web development through an
+      internship opportunity that allowed for me to grow both as a person and professionally. From here, I have worked
+      over the summer in the years following, allowing for me to gain many valuable skills, both in programming and
+      outside of it.
     </p>
-    <p class="text-center mb-5">
+    <p class="indent-8 mb-5">
       Outside of my coursework and internship work, I am also the vice president of the Waffle House Club at The Ohio
-      State University, a position that I've held since 2022.
+      State University, a position that I've held since 2022. In addition to this, I would describe myself as an avid
+      guitar player, with several years of continuous practice.
     </p>
     <p class="text-center mb-5">
       You can check out my projects <NuxtLink to="/projects" class="text-blue-600 underline">here</NuxtLink>.
