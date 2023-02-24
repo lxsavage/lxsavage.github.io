f74e0b7378a23bd5a9395d180377465e02804b83
lxsavage <31578557+lxsavage@users.noreply.github.com>
2023-02-24 16:23:52 -0500

    Modified build config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default {
   mode: 'static',
-  router: {
-    base: '/lxsavage.github.io/',
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
   },
   telemetry: false,
   modules: [
-      '@nuxtjs/tailwindcss',
+    '@nuxtjs/tailwindcss',
   ],
   buildModules: [
-    '@nuxtjs/fontawesome'
+    '@nuxtjs/fontawesome',
   ],
   fontawesome: {
     icons: {
       solid: ['faLocationDot', 'faEnvelope'],
       brands: ['faGithub', 'faLinkedinIn']
     }
-  }
+  },
+  css: [
+    '@/assets/css/main.css',
+  ],
 }
